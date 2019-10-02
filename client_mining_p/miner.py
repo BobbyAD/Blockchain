014476450c2461f3a61dea934fac9287438b02f1
Robert Driskell <driskell.ba@gmail.com>
2019-10-02 18:20:25 -0500

    last_block route done

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -5,7 +5,20 @@ import sys
 
 
 # TODO: Implement functionality to search for a proof 
+def proof_of_work(self, block):
+    """
+    Simple Proof of Work Algorithm
+    Find a number p such that hash(last_block_string, p) contains 6 leading
+    zeroes
+    :return: A valid proof for the provided block
+    """
+    block_string = json.dumps(block, sort_keys=True).encode()
 
+    proof = 0
+    while self.valid_proof(block_string, proof) is False:
+        proof += 1
+    
+    return proof
 
 if __name__ == '__main__':
     # What node are we interacting with?
